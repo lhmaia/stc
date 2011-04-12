0faa02dec3eaed402d7ed9e5a6c6fdba68487a02
LuizHenrique <luizhenrique@luizhenrique-laptop.(none)>
2011-04-12 10:08:09 -0300

    organização dos arquivos

---
+++ scripts/teste_tempo.sh
@@ -0,0 +1,7 @@
+./stc -i entradas/twitter01m_wsw
+./stc -i entradas/twitter05m_wsw
+./stc -i entradas/twitter10m_wsw
+./stc -i entradas/twitter15m_wsw
+./stc -i entradas/twitter20m_wsw
+./stc -i entradas/twitter25m_wsw
+./stc -i entradas/twitter30m_wsw
---
+++ scripts/teste_tempo_wiki.sh
@@ -0,0 +1,7 @@
+./stc -i entradas/wiki01
+./stc -i entradas/wiki05
+./stc -i entradas/wiki10
+./stc -i entradas/wiki15
+./stc -i entradas/wiki20
+./stc -i entradas/wiki25
+./stc -i entradas/wiki30
